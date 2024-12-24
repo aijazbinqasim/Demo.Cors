74ce38add45999f69c7f29b96a08d04505883090
Aijaz Bin Qasim <aijazbinqasim@hotmail.com>
2024-12-24 22:02:12 +0500

    Add project files.

---
+++ Demo.Cors/Program.cs
@@ -0,0 +1,50 @@
+
+namespace Demo.Cors
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+
+            // Enabling CORS as default policy globally.
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyMethod();
+                });
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            // applying cors policy globally.
+            app.UseCors();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
